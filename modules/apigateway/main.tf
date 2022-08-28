d171bb78cebc3af12da48e7a0f90485fd60d74c3
Noel Wilson <jwnwilson88@gmail.com>
2022-08-28 23:02:41 +0100

    Adding apigateway deployment command

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -67,6 +67,8 @@ resource "aws_api_gateway_stage" "this" {
   deployment_id = aws_api_gateway_deployment.apideploy.id
   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
   stage_name    = var.environment
+  cache_cluster_enabled = false
+  cache_cluster_size = "0.5"
   xray_tracing_enabled = true
   
 }
