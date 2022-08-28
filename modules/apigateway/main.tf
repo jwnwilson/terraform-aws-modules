72c48421c4c6a22ba5b8728fe2ae952d169bc657
Noel Wilson <jwnwilson88@gmail.com>
2022-08-28 22:13:45 +0100

    Adding authorizer

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -7,6 +7,16 @@ data "aws_route53_zone" "api_zone" {
   name = var.domain
 }
 
+data "aws_lambda_function" "authorizer" {
+  function_name = var.authorizer_name
+}
+
+resource "aws_api_gateway_authorizer" "auth" {
+  name           = "auth"
+  rest_api_id    = aws_api_gateway_rest_api.apiLambda.id
+  authorizer_uri = data.aws_lambda_function.authorizer.invoke_arn
+}
+
 resource "aws_route53_record" "this" {
   name                     = aws_api_gateway_domain_name.this.domain_name
   type                     = "A"
@@ -87,7 +97,8 @@ resource "aws_api_gateway_method" "proxyMethod" {
    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
    resource_id   = aws_api_gateway_resource.proxy.id
    http_method   = "ANY"
-   authorization = "NONE"
+   authorization = "CUSTOM"
+   authorizer_id = aws_api_gateway_authorizer.auth.id
 }
 
 resource "aws_api_gateway_integration" "lambda" {
