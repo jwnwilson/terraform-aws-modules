aa4fe101db3ed309ed7803cc0410e60afc51e155
Noel Wilson <jwnwilson88@gmail.com>
2022-08-29 01:43:07 +0100

    Keeping docs and openapi endpoints in authorizer

--- modules/apigateway-authorizer/main.tf
+++ modules/apigateway-authorizer/main.tf
@@ -99,7 +99,35 @@ resource "aws_api_gateway_method" "proxyMethod" {
    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
    resource_id   = aws_api_gateway_resource.proxy.id
    http_method   = "ANY"
-   authorization = "NONE"
+   authorization = "CUSTOM"
+   authorizer_id = aws_api_gateway_authorizer.auth.id
+}
+
+
+resource "aws_api_gateway_resource" "docs" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+  path_part   = "docs"
+}
+
+resource "aws_api_gateway_method" "docs" {
+  rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+  resource_id   = aws_api_gateway_resource.docs.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_resource" "openapi_json" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+  path_part   = "openapi.json"
+}
+
+resource "aws_api_gateway_method" "openapi_json" {
+  rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+  resource_id   = aws_api_gateway_resource.openapi_json.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -111,6 +139,27 @@ resource "aws_api_gateway_integration" "lambda" {
    type                    = "AWS_PROXY"
    uri                     = var.lambda_invoke_arn
 }
+
+resource "aws_api_gateway_integration" "docs" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_method.docs.resource_id
+  http_method = aws_api_gateway_method.docs.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_integration" "openapi_json" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_method.openapi_json.resource_id
+  http_method = aws_api_gateway_method.openapi_json.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_invoke_arn
+}
+
 
 resource "aws_api_gateway_method" "proxy_root" {
    rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
